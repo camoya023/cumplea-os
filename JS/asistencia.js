e77ea7b4cef85ec45e02473d1355d7d917601121
camoya023 <129466975+camoya023@users.noreply.github.com>
2023-10-16 17:01:21 -0500

    Add files via upload

--- JS/asistencia.js
+++ JS/asistencia.js
@@ -1,69 +1,46 @@
-window.onload = function () {
-  var icon_add_inv = document.getElementById("icon-add__inv");
-  var icon_add_acom1 = document.getElementById("icon-add__acom1");
-  var icon_rem_acom1 = document.getElementById("icon-rem__acom1");
-  var icon_add_acom2 = document.getElementById("icon-add__acom2");
-  var icon_rem_acom2 = document.getElementById("icon-rem__acom2");
-  var acompa1 = document.getElementById("acompa1");
-  var acompa2 = document.getElementById("acompa2");
-  //   var elementoOculto = document.getElementById("elemento-oculto");
+function generarRecibo() {
+  const titular = "Conjunto Residencial GRANATE"
+  const cuenta = "116053398"
+  const torreInput = document.getElementById('torre').value;
+  const apartamento = document.getElementById('apartamento').value;
+  const nombre = document.getElementById('nombre').value;
+  const telefono = document.getElementById('telefono').value;
+  const valor = document.getElementById('valor').value;
+  const total = valor.value;
+  const canvas = document.getElementById('recibo');
+  const ctx = canvas.getContext('2d');
+  const image = new Image();
+  const torreValues = { "1A": 11, "1B": 12, "1C": 13, "1D": 14, "2A": 21, "2B": 22, "2C": 23, "2D": 24 };
 
-  icon_add_inv.addEventListener("click", function () {
-    if (acompa1.style.display === "none") {
-      acompa1.style.display = "block";
-      icon_add_inv.style.display = "none";
-    } else {
-      acompa1.style.display = "none";
-      icon_rem_acom1.style.display = "none";
-    }
-  });
+  // Reemplaza 'nombre-de-tu-imagen.png' con la URL de tu imagen base
+  image.src = './/Assets/Images/cuenta-recaudo-avvillas.png';
 
-  icon_add_acom1.addEventListener("click", function () {
-    if (acompa2.style.display === "none") {
-      acompa2.style.display = "block";
-      icon_rem_acom1.style.display = "inline-block";
-      // icon_rem_acom1.style.display = "inline-block";
-      icon_add_acom1.style.display = "none";
-      icon_add_acom2.style.display = "none";
-    } else {
-      acompa2.style.display = "none";
-      icon_rem_acom1.style.display = "none";
-    }
-  });
+  image.onload = function () {
+      canvas.width = image.width;
+      canvas.height = image.height;
+      ctx.drawImage(image, 0, 0);
 
-  icon_rem_acom2.addEventListener("click", function () {
-    if (acompa2.style.display === "block") {
-      acompa2.style.display = "none";
-      icon_add_acom1.style.display = "inline-block";
-      // icon_add_acom1.style.display = "inline-block";
-      icon_rem_acom1.style.display = "none";
-    } else {
-      acompa2.style.display = "none";
-      icon_rem_acom1.style.display = "none";
-    }
-  });
+      // Obtén el valor numérico de la torre
+      const torre = torreValues[torreInput] || 0;
 
-  icon_rem_acom1.addEventListener("click", function () {
-    if (acompa1.style.display === "block") {
-      acompa1.style.display = "none";
-      icon_rem_acom2.style.display = "none";
-      icon_add_acom2.style.display = "inline-block";
-      // icon_add_acom1.style.display = "inline-block";
-      // icon_rem_acom2.style.display = "none";
-    } else {
-      acompa1.style.display = "none";
-      icon_rem_acom1.style.display = "none";
-    }
-  });
+      // Configura los valores en el canvas
+      ctx.font = '16px Arial';
+      ctx.fillStyle = 'red';
+      ctx.fillText(`${titular}`, 40, 115)
+      ctx.fillText(`${cuenta}`, 500, 115)
+      ctx.fillText(`${apartamento}`, 80, 188);
+      ctx.fillText(`${torre}`, 450, 188);
+      ctx.fillText(`${nombre}`, 40, 340);
+      ctx.fillText(`${telefono}`, 250, 340);
+      ctx.fillText(`${valor}`, 600, 333);
+      ctx.fillText(`${valor}`, 600, 355);
+      
 
-  icon_add_acom2.addEventListener("click", function () {
-    if (acompa1.style.display === "none") {
-      acompa1.style.display = "block";
-      icon_add_acom2.style.display = "none";
-      icon_rem_acom2.style.display = "inline-block";
-    } else {
-      acompa1.style.display = "none";
-      icon_rem_acom1.style.display = "none";
-    }
-  });
-};
+      // Muestra el botón de descarga
+      const descargar = document.getElementById('descargar');
+      descargar.style.display = 'inline';
+
+      // Descarga la imagen generada
+      descargar.href = canvas.toDataURL('image/png');
+  };
+}
